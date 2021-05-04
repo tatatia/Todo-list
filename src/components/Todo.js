6c229014ceea07ee877053138dc953710fb7522b
tatatia <thetattianochka@gmail.com>
2021-05-05 00:19:39 +0300

    final stage

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,14 +1,26 @@
 import React, { useState } from 'react'
 import TodoForm from "./TodoForm"
-import TodoList from './TodoList'
+import { RiCloseCircleLine } from "react-icons/ri"
+import { TiEdit } from "react-icons/ti"
 
-function Todo() {
+function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
     const [edit, setEdit] = useState({
         id: null,
         value: ""
     })
 
-    return TodoList.map((todo, index) => (
+    const submitUpdate = value => {
+        updateTodo(edit.id, value)
+        setEdit({
+            id: null,
+            value: ""
+        })
+    }
+
+    if (edit.id) {
+        return <TodoForm edit={edit} onSubmit={submitUpdate} />
+    }
+    return todos.map((todo, index) => (
         <div
             className={todo.isComplete ? "todo-row complete" : "todo-row"}
             key={index}
@@ -17,7 +29,14 @@ function Todo() {
                 {todo.text}
             </div>
             <div className="icons">
-                
+                <RiCloseCircleLine
+                    onClick={() => removeTodo(todo.id)}
+                    className="delete-icon"
+                />
+                <TiEdit
+                    onClick={() => setEdit({ id: todo.id, value: todo.text })}
+                    className="edit-icon"
+                />
             </div>
         </div>
     ))
